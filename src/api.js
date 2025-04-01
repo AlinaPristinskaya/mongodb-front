cbdc8d404cf35df73bb1f495d202f3a410cd53d5
Alina Prystynska <pristinskayaalina9@gmail.com>
2025-04-01 17:42:12 +0100

    55

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-//const instance = axios.create({
-   // baseURL: 'http://localhost:8000'
-//});
+
+
+
 
 const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://mongodb-back-ts6w.onrender.com'  // URL для Render (поменяйте на ваш актуальный)
-        : 'http://localhost:8000' // URL для локальной разработки
+    baseURL: 'https://mongodb-back-ts6w.onrender.com' // или локально 'http://localhost:10000'
 });
 
 
